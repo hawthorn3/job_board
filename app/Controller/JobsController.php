89c79bac8573dc4659e39899f324f7a760569dd9
hawthorn3 <r.tinkova@gmail.com>
2018-01-22 01:46:13 +0100

    mail + token
    Added send_mail method;
    however cannot configure the gmail smtp server;
    token is generated when new entry is submitted

--- app/Controller/JobsController.php
+++ app/Controller/JobsController.php
@@ -20,6 +20,18 @@ class JobsController extends AppController {
 		}
 		$this->set('job', $job);
 	}
+/*	
+	public function send_mail() {
+        $message = 'Hallo,/n Ihre Anzeige kann unter den folgenden Link bearbeitet werden: ';
+		$link = $this->request->host() . $this->webroot .'jobs/view/'. $this->request->data['Job']['id'] .'?token='. $this->request->data['Job']['token'];
+        App::uses('CakeEmail', 'Network/Email');
+        $Email = new CakeEmail('gmail');
+        $Email->from('jobs@job-board.com');
+        $Email->to($this->request->data['Job']['contact_mail']);
+        $Email->subject('Ihr Jobangebot wurde angelegt.');
+		$Email->send($message . $link);
+	}
+*/
 
 	public function add() {
 		// if the HTTP method of the request was POST, it saves the data using the Job model
@@ -27,10 +39,14 @@ class JobsController extends AppController {
 			$this->Job->create();
 			if ($this->Job->save($this->request->data)) {
 				$this->Flash->success(__('Der Job wurde erfolgreich angelegt.'));
+		//		$this->send_mail();
+				$job = $this->Job->findById($this->Job->getInsertId());
+				$this->Session->setFlash('Mail sent '.$this->request->host() . $this->webroot .'jobs/view/'. $job['Job']['id'] .'?token='. $job['Job']['token'],'default',array('class'=>'alert alert-success'));
 				return $this->redirect(array('action' => 'index'));
 			}
 			$this->Flash->error(__('Der Job konnte nicht gespeichert werden.'));
 		}
+		$this->Session->setFlash('Mail sent '.$this->request->data['Job']['token'],'default',array('class'=>'alert alert-success'));
 	}
 
 	public function edit($id = null) {
@@ -74,4 +90,6 @@ class JobsController extends AppController {
 
 		return $this->redirect(array('action' => 'index'));
 	}
+	
+
 }
